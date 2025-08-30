4caf50f2fd29f38b103ad5c0b8cab7d8db8b99e7
ma852lu <rodrigosbfilho@gmail.com>
2025-08-29 23:02:01 -0300

    tipos

---
+++ tipos.py
@@ -0,0 +1,14 @@
+#print(str(7) + ('brutal'))
+
+var = 1
+var2 = 10.4
+var = 15.2
+
+#print(type(1), type(1.1), type('brutal'))
+#print('brutal' + '7')
+
+#lista = ['brutal', 'batata', 'briar']
+#lista2 = [1,2,3,4]
+
+resultado = var + var2
+print(resultado)
