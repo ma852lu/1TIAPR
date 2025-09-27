f841ac34da73e5270518089e851d5c49c6f3ff0c
ma852lu <rodrigosbfilho@gmail.com>
2025-09-26 23:03:35 -0300

    bosta

---
+++ Aulas/Aula04/Matriz.py
@@ -0,0 +1,13 @@
+matriz = [
+    [1,2,3]
+    [4,5,6]
+    [7,8,9]
+]
+'''
+valor = matriz [0][0]
+print(valor+10)
+'''
+for linha in matriz:
+    for elemento in linha:
+        print(elemento)
+    print('---')
