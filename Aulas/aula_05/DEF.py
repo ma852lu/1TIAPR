9c6664e182ad52ba1a215cccdd47fb8d11c38f78
ma852lu <rodrigosbfilho@gmail.com>
2025-10-03 22:45:35 -0300

    bosta

---
+++ Aulas/aula_05/DEF.py
@@ -0,0 +1,19 @@
+def saudar(nome):
+    return(f'ola, {nome}')
+
+saudar('bosta')
+
+def saudar2(nome):
+    return f'oi {nome}'
+
+print(saudar2)
+
+def soma(a, b):
+    return a + b
+
+resultado = soma(7, 7)
+print(f'a soma é {soma}')
+
+
+
+
